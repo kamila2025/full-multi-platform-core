fc23c165d63443cbd4e35f38800dbef903e1aeb8
jianqi <jianqi810@gmail.com>
2025-09-05 14:39:52 +0800

    feat: 租戶會員管理功能

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->text('parameter')->nullable()->comment('額外參數');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
